7548c6b34f6dba3fb67f0b52e3630d50caf7e40e
oldmanpushcart@gmail.com <oldmanpushcart@gmail.com>
2014-10-04 04:27:20 +0800

    回退MappingDataSource

--- src/main/java/com/github/ompc/laser/common/LaserUtils.java
+++ src/main/java/com/github/ompc/laser/common/LaserUtils.java
@@ -10,6 +10,7 @@ public class LaserUtils {
 
     /**
      * 从size/3字符开始去掉size/3个字符，除法向下取整
+     *
      * @param data 原数据
      * @return 处理后数据
      */
@@ -18,8 +19,27 @@ public class LaserUtils {
         final int sub = size / 3;
         final byte[] newData = new byte[size - sub];
         arraycopy(data, 0, newData, 0, sub);
-        arraycopy(data, sub+sub, newData, sub, newData.length-sub);
+        arraycopy(data, sub + sub, newData, sub, newData.length - sub);
         return newData;
+    }
+
+    /**
+     * 对字节数组进行逆序
+     *
+     * @param bricks
+     * @return
+     */
+    public static byte[] reverse(byte[] bricks) {
+        if (null != bricks && bricks.length != 0) {
+            byte temp;
+            int len = bricks.length;
+            for (int i = 0; i < bricks.length / 2; i++) {
+                temp = bricks[i];
+                bricks[i] = bricks[len - i - 1];
+                bricks[len - i - 1] = temp;
+            }
+        }
+        return bricks;
     }
 
 }
