ea3548ed296bff39c7c297bcdc61f6a00b5f24f5
oldmanpushcart@gmail.com <oldmanpushcart@gmail.com>
2014-10-03 14:22:23 +0800

    修改启动器代码判断错误

--- src/main/java/com/github/ompc/laser/common/LaserLauncher.java
+++ src/main/java/com/github/ompc/laser/common/LaserLauncher.java
@@ -81,9 +81,9 @@ public class LaserLauncher {
 
     public static void main(String... args) throws IOException, InterruptedException {
 
-        if (args[0] == "server") {
+        if (args[0].equals("server")) {
             startServer(args);
-        } else if (args[0] == "client") {
+        } else if (args[0].equals("client")) {
             startClient(args);
         } else {
             throw new IllegalArgumentException("illegal args[0]=" + args[0]);
