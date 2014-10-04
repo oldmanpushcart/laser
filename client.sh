f4c0dd22986a577cdda9c67aa851dc0b67765107
oldmanpushcart@gmail.com <oldmanpushcart@gmail.com>
2014-10-04 20:58:38 +0800

    加大内存

--- client.sh
+++ client.sh
@@ -9,10 +9,5 @@ java \
     -Xmx8G \
     -Xms8G \
     -Xmn4G \
-    -XX:+UseConcMarkSweepGC \
-    -XX:CMSMaxAbortablePrecleanTime=5000 \
-    -XX:+CMSClassUnloadingEnabled \
-    -XX:CMSInitiatingOccupancyFraction=80 \
-    -XX:+UseCMSInitiatingOccupancyOnly \
     -jar ./target/laser-jar-with-dependencies.jar 'nioclient' ${SERVER_IP} ${SERVER_PORT} ${RESULT_DATAFILE_PATH} ./laser.properties
 
