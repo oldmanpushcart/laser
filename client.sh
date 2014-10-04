dd7e41cee82a6c26e5a50d5da73506c981599a90
oldmanpushcart@gmail.com <oldmanpushcart@gmail.com>
2014-10-04 20:52:18 +0800

    加大客户端内存

--- client.sh
+++ client.sh
@@ -6,8 +6,8 @@ typeset RESULT_DATAFILE_PATH=${3}
 
 java \
     -XX:+UseBiasedLocking\
-    -Xmx4G \
-    -Xms4G \
+    -Xmx8G \
+    -Xms8G \
     -Xmn2G \
     -jar ./target/laser-jar-with-dependencies.jar 'nioclient' ${SERVER_IP} ${SERVER_PORT} ${RESULT_DATAFILE_PATH} ./laser.properties
 
