2096ca1ad810bccccca833bf78f18f13640ec0d8
oldmanpushcart@gmail.com <oldmanpushcart@gmail.com>
2014-10-07 04:52:26 +0800

    降低自旋压力

--- src/main/java/com/github/ompc/laser/server/datasource/impl/PageDataSource.java
+++ src/main/java/com/github/ompc/laser/server/datasource/impl/PageDataSource.java
@@ -49,13 +49,13 @@ public class PageDataSource implements DataSource {
      * 页行数<br/>
      * 一页中总共有几行
      */
-    private final int PAGE_ROWS_NUM = 500000;
+    private final int PAGE_ROWS_NUM = 1000000;
 
     /*
      * 页码表大小<br/>
      * 一共有几页
      */
-    private final int PAGE_TABLE_SIZE = 32;
+    private final int PAGE_TABLE_SIZE = 12;
 
     /*
      * 页码表
