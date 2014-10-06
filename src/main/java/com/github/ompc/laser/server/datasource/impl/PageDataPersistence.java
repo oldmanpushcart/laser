ec9dfe65fe337276ad9831a9c05c1c1ecc8f90da
oldmanpushcart@gmail.com <oldmanpushcart@gmail.com>
2014-10-07 02:17:06 +0800

    降低自旋压力

--- src/main/java/com/github/ompc/laser/server/datasource/impl/PageDataPersistence.java
+++ src/main/java/com/github/ompc/laser/server/datasource/impl/PageDataPersistence.java
@@ -48,13 +48,13 @@ public class PageDataPersistence implements DataPersistence {
      * 页行数<br/>
      * 一页中总共有几行
      */
-    private final int PAGE_ROWS_NUM = 5000;
+    private final int PAGE_ROWS_NUM = 10000;
 
     /*
      * 页码表大小<br/>
      * 一共有几页
      */
-    private final int PAGE_TABLE_SIZE = 32;
+    private final int PAGE_TABLE_SIZE = 64;
 
 
     /*
