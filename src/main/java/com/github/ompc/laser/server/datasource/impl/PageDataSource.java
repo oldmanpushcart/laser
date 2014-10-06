9ce18673d619e0dbbacdbb18eda3f458bbb9bf98
oldmanpushcart@gmail.com <oldmanpushcart@gmail.com>
2014-10-07 02:05:19 +0800

    降低自旋压力

--- src/main/java/com/github/ompc/laser/server/datasource/impl/PageDataSource.java
+++ src/main/java/com/github/ompc/laser/server/datasource/impl/PageDataSource.java
@@ -94,15 +94,15 @@ public class PageDataSource implements DataSource {
      */
     @Override
     public Row getRow(Row row) throws IOException {
-//        if (isEOF) {
-//            if (null == row) {
-//                return EMPTY_ROW;
-//            } else {
-//                row.setLineNum(EMPTY_ROW.getLineNum());
-//                row.setData(EMPTY_ROW.getData());
-//                return row;
-//            }
-//        }
+        if (isEOF) {
+            if (null == row) {
+                return EMPTY_ROW;
+            } else {
+                row.setLineNum(EMPTY_ROW.getLineNum());
+                row.setData(EMPTY_ROW.getData());
+                return row;
+            }
+        }
 
         while (true) {
 
