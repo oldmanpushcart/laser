8051aed3050b1e558073ee0958b4847b5b8db16c
oldmanpushcart@gmail.com <oldmanpushcart@gmail.com>
2014-10-09 07:36:35 +0800

    异步加载文件

--- src/main/java/com/github/ompc/laser/server/NioLaserServer.java
+++ src/main/java/com/github/ompc/laser/server/NioLaserServer.java
@@ -235,11 +235,12 @@ public class NioLaserServer {
 
                                     if (key.isWritable()) {
                                         while (buffer.hasRemaining()) {
-                                            if( writableByteChannel instanceof CompressWritableByteChannel ) {
-                                                ((CompressWritableByteChannel)writableByteChannel).write(buffer,isEOF);
-                                            } else {
-                                                writableByteChannel.write(buffer);
-                                            }
+//                                            if( writableByteChannel instanceof CompressWritableByteChannel ) {
+//                                                ((CompressWritableByteChannel)writableByteChannel).write(buffer,isEOF);
+//                                            } else {
+//                                                writableByteChannel.write(buffer);
+//                                            }
+                                            writableByteChannel.write(buffer);
                                         }
                                         buffer.compact();
                                         state = DecodeState.FILL_BUFF;
