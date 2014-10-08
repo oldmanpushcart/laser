3b3ad3c7e021182e24e0ae54c732a334997c17f9
oldmanpushcart@gmail.com <oldmanpushcart@gmail.com>
2014-10-08 10:36:51 +0800

    去掉告警

--- src/main/java/com/github/ompc/laser/common/SocketUtils.java
+++ src/main/java/com/github/ompc/laser/common/SocketUtils.java
@@ -4,8 +4,6 @@ import com.github.ompc.laser.common.networking.GetDataReq;
 import com.github.ompc.laser.common.networking.GetDataResp;
 import com.github.ompc.laser.common.networking.GetEofResp;
 import com.github.ompc.laser.common.networking.Protocol;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -20,27 +18,26 @@ import static com.github.ompc.laser.common.LaserConstant.*;
  */
 public class SocketUtils {
 
-    private static final Logger log = LoggerFactory.getLogger(SocketUtils.class);
-
     /**
      * 格式化链接输出
      *
-     * @param socket
-     * @return
+     * @param socket socket链接
+     * @return 格式化后的信息
      */
     public static String format(Socket socket) {
-        final StringBuilder sb = new StringBuilder("[");
-        sb.append(socket.getLocalAddress()).append("->").append(socket.getRemoteSocketAddress());
-        sb.append("]");
-        return sb.toString();
+        return "["
+                + socket.getLocalAddress()
+                + "->"
+                + socket.getRemoteSocketAddress()
+                + "]";
     }
 
     /**
      * 读数据
      *
-     * @param dis
-     * @return
-     * @throws IOException
+     * @param dis 数据流
+     * @return 解析后的协议
+     * @throws IOException 网络出错
      */
     public static Protocol read(DataInputStream dis) throws IOException {
         final int type = dis.readInt();
@@ -61,8 +58,7 @@ public class SocketUtils {
                 final int len = dis.readInt();
 
                 while (dis.available() < len) {
-                    // TODO : spin wait for timeout
-//                    log.info("spin for read, available={};len={}",dis.available(), len);
+
                 }
 
                 final byte[] data = new byte[len];
@@ -70,7 +66,6 @@ public class SocketUtils {
                 resp.setData(data);
 
                 p = resp;
-//                log.debug("receive an RESP-GETDATA, lenNum={};len={}",resp.getLineNum(),resp.getData().length);
                 break;
             case PRO_RESP_GETEOF:
                 p = new GetEofResp();
@@ -86,9 +81,9 @@ public class SocketUtils {
     /**
      * 写数据
      *
-     * @param dos
-     * @param p
-     * @throws IOException
+     * @param dos 数据输出流
+     * @param p 协议报文
+     * @throws IOException 网络出错
      */
     public static void write(DataOutputStream dos, Protocol p) throws IOException {
 
@@ -100,16 +95,6 @@ public class SocketUtils {
             dos.writeInt(resp.getData().length);
             dos.write(resp.getData());
         }
-
-        /*
-        else if( p instanceof GetDataReq) {
-            // do nothing...
-        }
-
-        else if( p instanceof GetEofResp) {
-            // do nothing...
-        }
-        */
 
     }
 
